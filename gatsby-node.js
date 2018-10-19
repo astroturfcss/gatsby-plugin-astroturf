5fb1c7e438ea998cb2c752d19d4d9776c9e6ac64
Jason Quense <monastic.panic@gmail.com>
2018-10-19 12:41:20 -0400

    Simplify plugin and allow setting extension

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,23 +1,24 @@
 // match with Gatsby's CSS Modules rule
 const EXTENSION = '.module.css'
 
-const findJsRule = ({ config, rules }) =>
-  config.module.rules.find(({ test }) => test.source === rules.js().test.source)
 
 exports.onCreateWebpackConfig = (
   { actions, getConfig, rules },
   pluginOptions
 ) => {
-  const config = getConfig()
-
-  const jsRule = findJsRule({ config, rules })
-  const astroturfJsLoader = {
-    loader: 'astroturf/loader',
-    options: Object.assign({}, pluginOptions, {
-      extension: EXTENSION,
-    }),
-  }
-  jsRule.use.push(astroturfJsLoader)
-
-  actions.replaceWebpackConfig(config)
+  actions.setWebpackConfig({
+    module: {
+      rules: [
+        {
+          test: /\.jsx?$/,
+          use: [
+            {
+              loader: 'astroturf/loader',
+              options: Object.assign({ extension: EXTENSION }, pluginOptions),
+            },
+          ],
+        },
+      ],
+    },
+  });
 }
